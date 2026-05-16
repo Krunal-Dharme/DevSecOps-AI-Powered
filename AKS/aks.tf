cf2162ff2288799a53097431a44e73a89a777d80
Krunal-Dharme <110104854+Krunal-Dharme@users.noreply.github.com>
2026-05-16 21:21:03 +0530

    Update aks.tf

--- AKS/aks.tf
+++ AKS/aks.tf
@@ -1,1 +1,26 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "quantam-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "quantamaks"
 
+  default_node_pool {
+    name                = "systempool"
+    node_count         = 2
+    vm_size            = "Standard_D2s_v3"
+
+    vnet_subnet_id     = azurerm_subnet.subnet.id
+
+    enable_auto_scaling = true
+    min_count          = 2
+    max_count          = 3
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
