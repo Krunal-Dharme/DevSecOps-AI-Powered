870eced11944aa69ade2b0bfb5f5a6273c035c10
Krunal-Dharme <110104854+Krunal-Dharme@users.noreply.github.com>
2026-06-11 20:33:14 +0530

    Update aks.tf

--- AKS/aks.tf
+++ AKS/aks.tf
@@ -3,11 +3,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "quantamaks"
+  role_based_access_control_enabled = true
 
   default_node_pool {
     name                 = "systempool"
     node_count           = 1
-    vm_size              = "Standard_B2s_v2"
+    vm_size              = "Standard_D2als_v7"
     vnet_subnet_id       = azurerm_subnet.subnet.id
 
     enable_auto_scaling  = true
